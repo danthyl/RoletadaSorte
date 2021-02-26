b200fe4815e7c2ff56201ff0b1ef699ff9b1bad4
Daniel Ohata <danthyl@gmail.com>
2021-02-26 18:33:44 -0300

    update

---
+++ app/src/main/java/com/example/roletadasorte/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.roletadasorte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void roleta(View view) {
+        TextView texto = findViewById(R.id.Resultado);
+        int x = new Random().nextInt(11);
+        texto.setText("Número: " + x);
+
+    }
+}
